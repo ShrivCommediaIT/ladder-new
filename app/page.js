5424f7741a7923cebf702861db97c40f0a8b81e2
shriv <shrivcommediait10@gmail.com>
2026-05-21 18:20:17 +0530

    added talent board list in landing page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import ChooseGame, { GameSelect } from "@/components/pages/games/GameSelect";
 import LadderBenefits from "@/components/pages/payment/LadderBenefits";
 import PlanHeading from "@/components/pages/payment/PlanHeading";
 import OnboardingFlow from "@/components/shared/OnboardingFlow";
+import PerformanceDatabase from "@/components/shared/PerformanceDatabase";
 
 
 export default function Home() {
@@ -13,6 +14,10 @@ export default function Home() {
           {/* <GameSelect /> */}
             <div>
               <PlanHeading />
+            </div>
+
+            <div className="bg-[#05070f] border-t border-b border-white/5 relative z-10">
+              <PerformanceDatabase />
             </div>
 
             <div>
