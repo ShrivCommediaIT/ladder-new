d940976248edc522a384b9855c3c03b98d546410
shriv <shrivcommediait10@gmail.com>
2026-07-06 13:29:16 +0530

    return to the redirection link after payment test 2

--- components/pages/users/negativeLeaderBoardUser.js
+++ components/pages/users/negativeLeaderBoardUser.js
@@ -399,6 +399,15 @@ const NegativeLeaderboardUser = ({ ladderId: propLadderId, onActionsChanged }) =
                     console.error("Failed to post message to BroadcastChannel:", e);
                   }
 
+                  // Auto-close checkout tab to return user to original tab
+                  setTimeout(() => {
+                    try {
+                      window.close();
+                    } catch (err) {
+                      console.error("Failed to close window", err);
+                    }
+                  }, 1500);
+
                   // Refresh page URL to clear query params
                   const url = new URL(window.location.href);
                   url.searchParams.delete("payment_status");
