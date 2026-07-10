e57328ae1de0e3f266463b04bf76f9c67661f54c
shriv <shrivcommediait10@gmail.com>
2026-07-10 16:06:28 +0530

    issue resolved test 4

--- components/pages/payment/PlanHeading.jsx
+++ components/pages/payment/PlanHeading.jsx
@@ -636,9 +636,6 @@ export default function PlanHeading() {
                 </div>
               )}
             </div>
-
-
-
             <h1
               className="animate-fade-in-up mt-6 mb-8 text-5xl font-extrabold leading-[1.1] tracking-tight text-foreground md:text-7xl"
               style={{ animationDelay: "0.05s" }}
